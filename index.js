e7ac0dc44049321c0da7a432f92139e6fe380679
OS <os@MacBook-Pro-de-OS.local>
2026-05-08 17:22:42 -0400

    docs: project setup

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const title = document.createElement('h1')
-title.innerText = 'hola hades'
-
-
-const button = document.createElement
-('button')
-button.innerText = 'click me'
-
-button.addEventListener('click', function
-    (){
-        title.innerText = 'hola hades, actualizado'
-        alert('se realizo un click')
-    }
-)
-
-document.body.append(title)
-document.body.append(button)
+console.log('hola mundo')
